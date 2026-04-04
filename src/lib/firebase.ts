8b9e5e481477038872742ce6d52b63d37bc190c0
Surge-Ogiemudia <pogiemudia@gmail.com>
2026-04-04 06:10:18 +0100

    sync: src/lib/firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyABreq9YVCdK7k_t6bShsigK8Rmiiz5Um0",
-  authDomain: "pharmastackx-a3beb.firebaseapp.com",
-  projectId: "pharmastackx-a3beb",
-  storageBucket: "pharmastackx-a3beb.appspot.com",
-  messagingSenderId: "438683670037",
-  appId: "1:438683670037:web:1424ea0ad070281c5de01c",
-  measurementId: "G-DDV4NQT40E"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
