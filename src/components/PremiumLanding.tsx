f54cd11b69b823cc84b493b037dda619ca1dc549
Surge-Ogiemudia <pogiemudia@gmail.com>
2026-06-12 16:07:29 +0100

    fix: bump synkk download link to 1.1.6

--- src/components/PremiumLanding.tsx
+++ src/components/PremiumLanding.tsx
@@ -282,7 +282,7 @@ export default function PremiumLanding({ onSearchClick, onPharmacistClick, user,
                 
                 <Button
                   component="a"
-                  href="https://github.com/Surge-Ogiemudia/synkk-downloads/releases/download/v1.1.5/Synkk-Setup-1.1.5.exe"
+                  href="https://github.com/Surge-Ogiemudia/synkk/releases/download/v1.1.6/synkk-Setup-1.1.6.exe"
                   variant="contained"
                   sx={{ 
                     bgcolor: '#fff', 
