4a8fc1b25546d22a2ea7b2bdc669a19170350fd8
Surge-Ogiemudia <pogiemudia@gmail.com>
2026-06-22 09:59:56 +0100

    chore: update Synkk download link to v1.2.0

--- src/components/PremiumLanding.tsx
+++ src/components/PremiumLanding.tsx
@@ -282,7 +282,7 @@ export default function PremiumLanding({ onSearchClick, onPharmacistClick, user,
                 
                 <Button
                   component="a"
-                  href="https://github.com/Surge-Ogiemudia/synkk-downloads/releases/download/v1.1.6/Synkk-Setup-1.1.6.exe"
+                  href="https://github.com/Surge-Ogiemudia/synkk-downloads/releases/download/v1.2.0/Synkk-Setup-1.2.0.exe"
                   variant="contained"
                   sx={{ 
                     bgcolor: '#fff', 
