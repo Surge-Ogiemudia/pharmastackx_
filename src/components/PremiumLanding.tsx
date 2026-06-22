8e43748bd1e38ccfbe98865b2b82e0dfca397dbd
Surge-Ogiemudia <pogiemudia@gmail.com>
2026-06-22 10:23:09 +0100

    fix: update Synkk download banner to correct v1.2.0 filename

--- src/components/PremiumLanding.tsx
+++ src/components/PremiumLanding.tsx
@@ -282,7 +282,7 @@ export default function PremiumLanding({ onSearchClick, onPharmacistClick, user,
                 
                 <Button
                   component="a"
-                  href="https://github.com/Surge-Ogiemudia/synkk-downloads/releases/latest/download/Synkk-Setup.exe"
+                  href="https://github.com/Surge-Ogiemudia/synkk-downloads/releases/latest/download/Synkk-Setup-1.2.0.exe"
                   variant="contained"
                   sx={{ 
                     bgcolor: '#fff', 
